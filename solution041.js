e7f3968241c519cafdd3a2e833034b30ee531345
Jon Wright <jon.wright9080@gmail.com>
2023-05-09 10:55:28 -0500

    sort the odd

--- solution041.js
+++ solution041.js
@@ -7,3 +7,7 @@
 // [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
 // [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 
+function sortArray(array) {
+    const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+    return array.map((x) => x % 2 ? odd.shift() : x);
+}
